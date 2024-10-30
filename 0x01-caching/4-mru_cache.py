1e6773784c8c63f27d6a2c6542e6c0b56c8625f0
ayoubleass <ayoublea@gmail.com>
2024-10-30 15:58:36 +0000

    CREATE file : 4-mru_cache.py

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+This module has a class that inherits from
+BaseCaching and is a caching system.
+"""
+from base_caching import BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """
+    This class inherits from BaseCaching and is a caching system.
+    """
+
+    def __init__(self):
+        """
+        initialize the cache
+        """
+        super().__init__()
+
+    def put(self, key, item):
+        """
+        Add new item to the cache dict.
+        """
+        if key is None or item is None:
+            return
+        if key in self.cache_data:
+            del self.cache_data[key]
+        if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
+            mru_key = list(self.cache_data.keys())[-1]
+            print("DISCARD:", mru_key)
+            del self.cache_data[mru_key]
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        returns an item in the cashe dict.
+        """
+        if key is None or key not in self.cache_data:
+            return
+        item = self.cache_data[key]
+        del self.cache_data[key]
+        self.cache_data[key] = item
+        return self.cache_data[key]
